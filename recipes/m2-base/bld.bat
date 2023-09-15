2637f6c7fcd952c9af3640eac491bb5033d0120c
Isuru Fernando <isuruf@gmail.com>
2023-09-15 02:34:39 -0500

    Fix m2-base

--- recipes/m2-base/bld.bat
+++ recipes/m2-base/bld.bat
@@ -1,2 +1,1 @@
 mkdir %LIBRARY_PREFIX%
-xcopy %SRC_DIR%\binary\ %LIBRARY_PREFIX%\ /s /e /y
--- recipes/m2-base/build.sh
+++ recipes/m2-base/build.sh
@@ -1,3 +1,1 @@
 mkdir $PREFIX/Library
-cp -R $SRC_DIR/binary/* $PREFIX/Library/
-test -d $PREFIX/Library/usr
