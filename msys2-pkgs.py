b59c0f70d700c0768032bf776ad822c59a8ec169
Isuru Fernando <isuruf@gmail.com>
2023-09-14 23:20:54 -0500

    script to generate files

--- msys2-pkgs.py
+++ msys2-pkgs.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import subprocess
 import os
+import shutil
 
 index_url = 'https://repo.msys2.org/msys/x86_64/'
 
@@ -25,6 +26,9 @@ pkg_latest_ver = dict(get_pkgs())
 # for pkg, info in pkg_latest_ver.items():
 #    print(pkg, info)
 
+def get_info(pkginfo, desc):
+    return [line[len(f"{desc} = "):].strip() for line in pkginfo if line.startswith(f"{desc} = ")]
+
 def get_depends(pkg):
     info = pkg_latest_ver[pkg]
     basename = f"cache/{pkg}-{info}"
@@ -34,19 +38,47 @@ def get_depends(pkg):
     subprocess.check_call(["mkdir", "-p", "cache/tmp"])
     subprocess.check_call(["tar", "-xf", basename, "--directory=cache/tmp"])
     with open("cache/tmp/.PKGINFO") as f:
-        depends = [line[len("depend = "):].strip() for line in f.readlines() if line.startswith("depend = ")]
+        pkginfo = f.readlines()
+    depends = get_info(pkginfo, "depend")
     depends = [dep for dep in depends if not dep.startswith("pacman")]
-    return depends
+    license_text = get_info(pkginfo, "license")[0]
+    spdx = license_text[5:] if license_text.startswith("spdx:") else license_text
+    desc = get_info(pkginfo, "pkgdesc")[0]
+    url = get_info(pkginfo, "url")[0]
+    return depends, spdx, desc, url
 
 while to_process:
     pkg = to_process.pop()
-    depends = get_depends(pkg)
+    depends, spdx, desc, url = get_depends(pkg)
     for full_dep in depends:
         dep = full_dep.split(">")[0].split("=")[0].split("<")[0]
         dep = provides.get(dep, dep)
         if dep not in seen:
             to_process.add(dep)
-    seen[pkg] = depends
+    seen[pkg] = depends, spdx, desc, url
 
-for pkg, depends in seen.items():
+with open("template/meta.yaml") as f:
+    template = f.read()
+
+for pkg, (depends, spdx, desc, url) in seen.items():
     print(f"{pkg} {pkg_latest_ver[pkg]} {' '.join(depends)}")
+    subprocess.check_call(["mkdir", "-p", f"recipes/m2-{pkg}"])
+    info = pkg_latest_ver[pkg]
+    sha256 = subprocess.check_output(["sha256sum", f"cache/{pkg}-{info}"]).decode("utf-8").split(" ")[0]
+    meta = template
+    info = {
+        "name": pkg,
+        "version": ".".join(info.split("-")[:2] ),
+        "tarname": f"{pkg}-{info}",
+        "depends": "\n".join(f"    - m2-{dep}" for dep in depends),
+        "license": spdx,
+        "summary": desc,
+        "url": url,
+        "sha256": sha256,
+    }
+    for k, v in info.items():
+        meta = meta.replace(f"{{{{ {k} }}}}", v)
+    with open(f"recipes/m2-{pkg}/meta.yaml", "w") as f:
+        f.write(meta)
+    shutil.copy("template/build.sh", f"recipes/m2-{pkg}/build.sh")
+    shutil.copy("template/bld.bat", f"recipes/m2-{pkg}/bld.bat")
