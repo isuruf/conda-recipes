e63510cb2f41320ec63b60cf2b5e091f40e30041
Isuru Fernando <isuruf@gmail.com>
2023-09-14 23:20:39 -0500

    new recipes

---
+++ recipes/m2-libxml2/bld.bat
@@ -0,0 +1,2 @@
+mkdir %LIBRARY_PREFIX%
+xcopy %SRC_DIR%\binary\ %LIBRARY_PREFIX%\ /s /e /y
---
+++ recipes/m2-libzstd/build.sh
@@ -0,0 +1,3 @@
+mkdir $PREFIX/Library
+cp -R $SRC_DIR/binary/* $PREFIX/Library/
+test -d $PREFIX/Library/usr
