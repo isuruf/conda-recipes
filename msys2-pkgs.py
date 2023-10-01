074bd564cc7613c44b720fef8ab1bd47868dffd4
Isuru Fernando <isuruf@gmail.com>
2023-09-30 23:46:28 -0500

    wip

--- msys2-pkgs.py
+++ msys2-pkgs.py
@@ -30,6 +30,7 @@ def get_info(pkginfo, desc):
     return [line[len(f"{desc} = "):].strip() for line in pkginfo if line.startswith(f"{desc} = ")]
 
 def get_depends(pkg):
+    # download and extract binary
     info = pkg_latest_ver[pkg]
     basename = f"cache/{pkg}-{info}"
     url = f"{index_url}/{pkg}-{info}"
@@ -39,17 +40,33 @@ def get_depends(pkg):
     subprocess.check_call(["tar", "-xf", basename, "--directory=cache/tmp"])
     with open("cache/tmp/.PKGINFO") as f:
         pkginfo = f.readlines()
+
+    # download source
+    pkgbase = get_info(pkginfo, "pkgbase")[0]
+    pkgver = get_info(pkginfo, "pkgver")[0]
+    src1 = f"https://repo.msys2.org/msys/sources/{pkgbase}-{pkgver}.src.tar.zst"
+    src2 = f"https://repo.msys2.org/msys/sources/{pkgbase}-{pkgver}.src.tar.gz"
+    if not os.path.exists('src-cache/' + os.path.basename(src1)) and \
+            not os.path.exists('src-cache/' + os.path.basename(src2)):
+        try:
+            src_name = src1
+            subprocess.check_call(["wget", src1, "-O", 'src-cache/' + os.path.basename(src1)])
+        except subprocess.CalledProcessError:
+            src_name = src2
+            subprocess.check_call(["wget", src2, "-O", 'src-cache/' + os.path.basename(src2)])
+
+    # get dependencies
     depends = get_info(pkginfo, "depend")
     depends = [dep for dep in depends if not dep.startswith("pacman")]
     license_text = get_info(pkginfo, "license")[0]
     spdx = license_text[5:] if license_text.startswith("spdx:") else license_text
     desc = get_info(pkginfo, "pkgdesc")[0]
     url = get_info(pkginfo, "url")[0]
-    return depends, spdx, desc, url
+    return depends, spdx, desc, url, src_name
 
 while to_process:
     pkg = to_process.pop()
-    depends, spdx, desc, url = get_depends(pkg)
+    depends, spdx, desc, url, src_name = get_depends(pkg)
     for i, full_dep in enumerate(depends):
         dep = full_dep.split(">")[0].split("=")[0].split("<")[0]
         cond = full_dep[len(dep):]
@@ -60,12 +77,21 @@ while to_process:
             depends[i] = dep
         if dep not in seen:
             to_process.add(dep)
-    seen[pkg] = depends, spdx, desc, url
+    seen[pkg] = depends, spdx, desc, url, src_name
 
 with open("m2-template/meta.yaml") as f:
     template = f.read()
 
-for pkg, (depends, spdx, desc, url) in seen.items():
+sources_template = """
+  - url:
+      - https://repo.msys2.org/mingw/ucrt64/mingw-w64-ucrt-x86_64-libwinpthread-git-{{ version }}-any.pkg.tar.zst
+      # archive since the package link above expires when new versions appear.
+      - https://github.com/conda-forge/msys2-recipes/releases/download/20230914-ucrt64/mingw-w64-ucrt-x86_64-libwinpthread-git-{{ version }}-any.pkg.tar.zst
+    sha256: 905db9ba4cf395e1d130903864cbf70240945b77e9d6af1cc00d343914d70fb7
+    folder: source-{{ name }}
+"""
+
+for pkg, (depends, spdx, desc, url, src_name) in seen.items():
     print(f"{pkg} {pkg_latest_ver[pkg]} {' '.join(depends)}")
     subprocess.check_call(["mkdir", "-p", f"recipes/m2-{pkg}"])
     info = pkg_latest_ver[pkg]
